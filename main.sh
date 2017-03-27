03720bd2496ca4b916d53754166577e2403eb3d0
potis <korfiatisp@gmail.com>
2017-03-27 10:15:45 -0500

    had an echo in the commnad that remained there after testing

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 touch panos.md 
 sleep 2
 
-find DICOM/ -type d -print0 | xargs -n 1 --max-procs 1 -0 echo ./ingest.py --db dataingested.db --t ingest.config
+find DICOM/ -type d -print0 | xargs -n 1 --max-procs 1 -0 ./ingest.py --db dataingested.db --t ingest.config
