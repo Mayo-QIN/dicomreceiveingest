679b001a898ac35e17c0434623ff25a80769b495
potis <Korfiatis.Panagiotis@mayo.edu>
2017-03-29 14:56:36 -0500

    new receive

--- radioreceiver.sh
+++ radioreceiver.sh
@@ -6,4 +6,4 @@ echo STARTING RECEIVER
 # storescp -v  --accept-all --promiscuous --sort-conc-studies STD 8832   -od DICOM/  -xcs './main.sh' -tos  100 
 
 # After talking to bill 
-storescp -v -aet receive --sort-conc-studies -pm +xa -od /DICOM/ 8832 -xcs './main.sh' -tos  100
+storescp -v -aet receive --sort-conc-studies -pm +xa -od /DICOM/ STD 8832 -xcs './main.sh' -tos  100
