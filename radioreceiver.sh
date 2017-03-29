f7cc2eed042ff17d27bd70433408d3f85104b4b3
potis <Korfiatis.Panagiotis@mayo.edu>
2017-03-29 11:21:30 -0500

    Corrected receiver

--- radioreceiver.sh
+++ radioreceiver.sh
@@ -3,4 +3,7 @@ source .bashrc
 echo STARTING FLASK SITE TO UPLOAD COSTUM CONFIGS
 nohup ./upload.py >website.log &
 echo STARTING RECEIVER
-storescp -v  --accept-all --promiscuous --sort-conc-studies STD 8832   -od DICOM/  -xcs './main.sh' -tos  100 
+# storescp -v  --accept-all --promiscuous --sort-conc-studies STD 8832   -od DICOM/  -xcs './main.sh' -tos  100 
+
+# After talking to bill 
+storescp -v  -pm +xa -od DICOM/ STD 8832 -xcs './main.sh' -tos  100 
